7433b25dfab2cdc4b48618a5028e2d4e73abfb3a
Ariel Kanevsky <ariel.kan88@gmail.com>
2018-02-14 23:01:57 +0200

    Second Attempt

---
+++ processverClient/src/main/java/Client.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import org.apache.commons.codec.digest.DigestUtils;
+import com.processver.model.ProcessStatus;
+
+public class Client {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
